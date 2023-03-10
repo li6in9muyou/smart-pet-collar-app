f0b7e4098d8c888028e142af4b8b29aa06b186a5
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-10 18:10:32 +0800

    feat: round up measurements to 3 decimal places by default

--- src/lib/FormattedNumericReading.svelte
+++ src/lib/FormattedNumericReading.svelte
@@ -2,9 +2,10 @@
 	export let unit = '';
 	export let value = null;
 	export let label = '读数';
+	export let decimalPlaces = 3;
 </script>
 
 <div>
-	{label}：{value === null ? '不可用' : value}
+	{label}：{value === null ? '不可用' : value.toFixed(decimalPlaces)}
 	{unit}
 </div>
