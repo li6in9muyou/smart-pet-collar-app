34e2634347ee8cd04fb60e2f5315807d88fe4852
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-12 13:03:45 +0800

    linting

--- src/lib/chartjs.config.js
+++ src/lib/chartjs.config.js
@@ -20,7 +20,7 @@ export default () => ({
 				data: [],
 
 				lineTension: 0.3,
-				backgroundColor: 'rgba(225, 204,230, .3)',
+				backgroundColor: 'rgba(225, 204, 230, .3)',
 				borderColor: 'rgb(205, 130, 158)',
 				borderCapStyle: 'butt',
 				borderDash: [],
@@ -31,7 +31,7 @@ export default () => ({
 				pointBorderWidth: 5,
 				pointHoverRadius: 5,
 				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
-				pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+				pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
 				pointHoverBorderWidth: 2,
 				pointRadius: 1,
 				pointHitRadius: 10
