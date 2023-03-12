30e41e65e2f6e99c9abefb47dcb6525065f8a800
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-12 11:10:39 +0800

    use different mock data

--- src/routes/health-monitor/+page.svelte
+++ src/routes/health-monitor/+page.svelte
@@ -33,6 +33,33 @@
 		]
 	};
 
+	const data2 = {
+		labels: new Array(30).fill(''),
+		datasets: [
+			{
+				label: '',
+				data: [],
+
+				lineTension: 0.3,
+				backgroundColor: 'rgba(225, 204,230, .3)',
+				borderColor: 'rgb(205, 130, 158)',
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: 'rgb(205, 130, 158)',
+				pointBackgroundColor: 'rgb(255, 255, 255)',
+				pointBorderWidth: 5,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+				pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10
+			}
+		]
+	};
+
 	const op = {
 		plugins: {
 			legend: { display: false }
@@ -60,7 +87,8 @@
 		bloodOxygenLevel = latestValueOf('百分之');
 		bloodOxygenLevelWindow = windowOf('百分之');
 
-		heartRateWindow.subscribe((values) => (data.datasets[0].data = values));
+		bloodOxygenLevelWindow.subscribe((values) => (data.datasets[0].data = values));
+		heartRateWindow.subscribe((v) => (data2.datasets[0].data = v));
 	});
 </script>
 
@@ -83,7 +111,7 @@
 	</div>
 	<div class="chart">
 		<div>
-			<Line {data} options={op} />
+			<Line data={data2} options={op} />
 		</div>
 		<span>HbO2</span>
 	</div>
