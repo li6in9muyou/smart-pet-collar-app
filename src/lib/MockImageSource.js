fe4fc190616750210f7edd87ff27095cbc094402
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-14 15:29:19 +0800

    fix: clearInterval

--- src/lib/MockImageSource.js
+++ src/lib/MockImageSource.js
@@ -2,8 +2,9 @@ import { readable } from 'svelte/store';
 
 export default readable('https://placehold.jp/320x320.png?text=capture-1', (set) => {
 	let i = 2;
-	setInterval(() => {
+	const handle = setInterval(() => {
 		set('https://placehold.jp/320x320.png?text=capture-' + i);
 		i += 1;
 	}, 1100);
+	return () => clearInterval(handle);
 });
