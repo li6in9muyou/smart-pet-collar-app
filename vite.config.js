a23552caa06c20cd4be615162e10c73341366a66
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-23 22:17:04 +0800

    add and bundle alibabacloud-iot-device-sdk

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			protocolImports: true
+		})
+	]
 };
 
 export default config;
