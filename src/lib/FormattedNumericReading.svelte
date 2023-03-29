512584d6e4870be963839ebc1ebbad0fd53b3b71
li6in9muyou <libq5520@mails.jlu.edu.cn>
2023-03-29 16:58:11 +0800

    fix: avoid showing NaN

--- src/lib/FormattedNumericReading.svelte
+++ src/lib/FormattedNumericReading.svelte
@@ -3,9 +3,16 @@
 	export let numberStream = null;
 	export let unit = '';
 	export let decimalPlaces = 3;
+
+	let text = '不可用';
+	$: {
+		if (numberStream !== null && !isNaN($numberStream)) {
+			text = $numberStream.toFixed(decimalPlaces);
+		}
+	}
 </script>
 
 <div>
-	{label}：{numberStream === null ? '不可用' : $numberStream.toFixed(decimalPlaces)}
+	{label}：{text}
 	{unit}
 </div>
